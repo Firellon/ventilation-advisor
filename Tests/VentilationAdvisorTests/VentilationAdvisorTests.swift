50455bf5df6f6136132c621f117d1d1b49d92776
Erik Basargin <basargin.erik@gmail.com>
2026-07-26 17:49:54 +0100

    Update package identity

--- Tests/VentilationAdvisorTests/VentilationAdvisorTests.swift
+++ Tests/VentilationAdvisorTests/VentilationAdvisorTests.swift
@@ -1,5 +1,6 @@
 import Testing
-@testable import ventilation_advisor
+
+@testable import VentilationAdvisor
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
